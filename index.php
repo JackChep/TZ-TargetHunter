83d34da55eed873dab3b1ff5a3c55a0178894449
Евгений <110800627+JackChep@users.noreply.github.com>
2024-03-31 04:03:54 +0500

    попытка сделать в цикле

--- index.php
+++ index.php
@@ -1,24 +1,41 @@
 <?php
+set_time_limit(0);
 
-$vkCommunityId = '166884737';
-$token = "YOUR_ACCESS_TOKEN";
+//71474323  - 250к
+//166317286  - 300
+//203077985  - 100к
+
+
+$vkCommunityId = '203077985';
+$token = "token";
+
+$requestParams = http_build_query([
+    'group_id' => $vkCommunityId,
+    'v' => '5.131',
+    'access_token' => $token,
+]);
+
+$qty_users = json_decode(file_get_contents("https://api.vk.com/method/groups.getMembers?$requestParams"), true);
+$qty = $qty_users['response']['count'];
 
 $requestBody = "
-var members = [];
-var offset = 0;
-var count = 1000;
-var response;
-do {
-    response = API.groups.getMembers({
-        group_id: $vkCommunityId,
-        offset: offset,
-        count: count,
-        fields: 'bdate, country, city'
-    });
-    members = members + response.items;
-    offset = members.length;
-} while (offset < response.count);
-return members;";
+    var members = [];
+    var offset = 0;
+    var response;
+    while ( offset <= 24000 ){
+        response = API.groups.getMembers({
+            group_id: $vkCommunityId,
+            offset: offset,
+            sort: 'id_asc',
+            fields: 'bdate,first_name'
+        });
+        members = members + response.items;
+        offset = members.length;
+        if(offset == $qty){
+            return members;
+        }
+    } ;
+    return members;";
 
 $requestParams = http_build_query([
     'code' => $requestBody,
@@ -26,7 +43,26 @@ $requestParams = http_build_query([
     'access_token' => $token,
 ]);
 
-$response = json_decode(file_get_contents("https://api.vk.com/method/execute?$requestParams"), true);
+$count = 0;
+$data['data'] = [];
 
+while( $count != $qty ){
 
+    $response = json_decode(file_get_contents("https://api.vk.com/method/execute?$requestParams"), true);
+
+    array_push($data['data'], $response['response']);
+
+    $count += count($response['response']);
+
+    //$data['data'] = $response['response'];
+
+}
+
+//$data['data'] = $response['response'];
+//$data['count'] = count($response['response']);
+$data['quantity_users'] = $qty_users['response']['count'];
+
+$el = json_encode($data);
 ?>
+
+<script>console.log(<?=$el?>)</script>
