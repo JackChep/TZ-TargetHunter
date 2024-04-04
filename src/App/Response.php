d12f03e616f521243e3c689d39e8202ce22d30c7
JackChep <dorovichg@mail.ru>
2024-04-04 07:49:08 +0500

    fix

--- src/App/Response.php
+++ src/App/Response.php
@@ -19,11 +19,35 @@ class Response{
             $this->{$key} = $value;
         }
 
+        if(isset($data->items)) {
+            $this->items = !isset($data->items) ? false : $data->items;
+        }
+
         $this->count = !isset($data->count) ? false : $data->count;
 
         if (is_array($data)) {
             $this->count = count($data);
             $this->data = $data;
+        }
+    }
+
+    public function getCount(){
+        return count($this->items);
+    }
+
+    
+    public function getById($id = false)
+    {
+        if (!$id) {
+            if (is_array($this->data)) {
+                return $this->data[0];
+            } elseif (isset($this->items) && $this->items !== false) {
+                return $this->items[0];
+            } else {
+                return $this->data;
+            }
+        } else {
+            return $this->data[$id];
         }
     }
 
